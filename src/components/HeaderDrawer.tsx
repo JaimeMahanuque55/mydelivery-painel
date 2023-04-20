fba15d55f1731a6d55de8f57fa72a7c115ab9e94
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2023-04-20 21:50:45 +0200

    creating the header drawer 2

--- src/components/HeaderDrawer.tsx
+++ src/components/HeaderDrawer.tsx
@@ -1,4 +1,5 @@
-import { Drawer, Box, Typography, Divider } from "@mui/material";
+import { Drawer, Box, Typography, Divider, List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import Link from "next/link";
 
 type Props = {
   open: boolean;
@@ -26,6 +27,34 @@ export const HeaderDrawer = ({ open, title, onClose, onLogout }: Props) => {
           {title}
         </Typography>
         <Divider />
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <Link href="/pedidos" style={{ color: '#000', textDecoration: 'none' }}>
+                <ListItemText primary="Pedidos" />
+              </Link>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <Link href="/produtos" style={{ color: '#000', textDecoration: 'none' }}>
+                <ListItemText primary="Produtos" />
+              </Link>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <Link href="/categorias" style={{ color: '#000', textDecoration: 'none' }}>
+                <ListItemText primary="Categorias" />
+              </Link>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton onClick={onLogout}>
+              <ListItemText primary="Sair" />
+            </ListItemButton>
+          </ListItem>
+        </List>
       </Box>
     </Drawer>
   )
