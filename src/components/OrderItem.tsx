6c2c4d8bf6565a5ba79008658c382937f5771e83
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2023-05-16 21:11:06 +0200

    Order: Creating the OrderItem 1

--- src/components/OrderItem.tsx
+++ src/components/OrderItem.tsx
@@ -1,14 +1,40 @@
 import { Order } from "@/types/Order";
-import { Box } from "@mui/material";
+import { OrderStatus } from "@/types/OrderStatus";
+import { Box, Button, Typography } from "@mui/material";
 
 type Props = {
   item: Order;
 }
 
 export const OrderItem = ({ item }: Props) => {
+
+  const getStatusBackground = (status: OrderStatus) => {
+    const statuses = {
+      preparing: '#2787BA',
+      sent: '#27BA3A',
+      delivered: '#999999'
+    }
+    return statuses[status]
+  }
+
   return (
-    <Box>
-      ...
+    <Box sx={{ border: '1px solid #EEE', color: '#FFF', borderRadius: 2, overflow: 'hidden' }}>
+      <Box sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        p: 1,
+        backgroundColor: getStatusBackground(item.status)
+      }}>
+        <Box>
+          <Typography component="p">{item.orderDate}</Typography>
+          <Typography component="p">{item.userName}</Typography>
+          <Button size="small" sx={{ color: '#FFF', p: 0 }}>Imprimir</Button>
+        </Box>
+        <Box>
+          <Typography component="p" sx={{ fontSize: 24 }}>#{item.id}</Typography>
+        </Box>
+      </Box>
     </Box>
   )
 }
